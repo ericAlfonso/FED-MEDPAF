e523d2e7057b7fce9daa6147b3ed5976a39260bf
RayVazquez <vremi909@gmail.com>
2019-04-11 19:10:03 -0500

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { IncidentComponent } from './components/incidents/incident.component';
+import { IncidentComponent } from './views/incidents/incident.component';
 
 @NgModule({
-<<<<<<< HEAD
   declarations: [AppComponent, IncidentComponent],
-  imports: [BrowserModule, AppRoutingModule, LayoutModule],
-=======
-  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
->>>>>>> first-release/integration
   providers: [],
   bootstrap: [AppComponent]
 })
